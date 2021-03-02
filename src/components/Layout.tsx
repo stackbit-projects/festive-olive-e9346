ac6f4d842aa0cee568f29258155f63190edbbe22
Stackbit Code Editor <support@stackbit.com>
2021-03-02 19:45:04 +0000

    Layout.tsx: changed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -49,7 +49,7 @@ const Layout = (props: LayoutProps) => {
           {/* Footer */}
           <Segment inverted vertical style={{ position: "absolute", bottom: 0, width: "100%" }}>
             <Container textAlign="center">
-              <p>DEsigned <Icon name="heart" /> by Gatsby 2.0</p>
+              <p>Designed <Icon name="heart" /> by Gatsby 2.0</p>
             </Container>
           </Segment>
         </Sidebar.Pusher>
